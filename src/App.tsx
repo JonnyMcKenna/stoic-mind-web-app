9417b825f8e39088c8ceac2994363f87637248a6
jonnymckenna <jonny_mck@hotmail.co.uk>
2022-07-23 19:43:42 +0100

    Add Adsense and google play link

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React, { useEffect, useReducer, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import data from "./quotes.json";
 import { QuoteProps } from "../types/genericTypes";
 import FadeIn from "react-fade-in";
+import { Adsense } from "@ctrl/react-adsense";
+import Footer from "./Footer";
+import logo from "./images/google-play.webp";
 
 function App() {
   const [quote, setQuote] = useState<QuoteProps>();
-
-  //TODO: Fix the fade in and out func when changing quotes
-  const [fadeIn, setFadeIn] = useState<Boolean>(true);
 
   useEffect(() => {
     const newQuote = generateNewQuote();
@@ -23,50 +23,40 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <div
-        className="App-body"
-        onClick={() => {
-          // setFadeIn(false);
-
-          // setTimeout(() => {
-          const newQuote = generateNewQuote();
-          setQuote(newQuote);
-          // }, 1000);
-
-          // setFadeIn(true);
-        }}
+    <div
+      className="App-body"
+      onClick={() => {
+        const newQuote = generateNewQuote();
+        setQuote(newQuote);
+      }}
+    >
+      <a
+        className="google-play-link"
+        target="_blank "
+        href="https://play.google.com/store/apps/details?id=com.jonnymckenna.stoicmind"
       >
+        <img src={logo} alt="Google Play Link for Stoic Mind" />
+      </a>
+      <div className="App-content">
         {quote && (
           <FadeIn delay={200} transitionDuration={1500}>
             <blockquote className="App-blockquote">
-              <p
-                className="App-quote"
-                // style={
-                //   fadeIn
-                //     ? { animation: "fadeIn 1s" }
-                //     : { animation: "fadeOut 1s" }
-                // }
-              >
-                {quote.text}
-              </p>
+              <p className="App-quote">{quote.text}</p>
               <div className="App-author">- {quote.author}</div>
             </blockquote>
           </FadeIn>
         )}
       </div>
-      <FadeIn delay={800} transitionDuration={1500}>
-        <footer className="App-footer">
-          Created by{" "}
-          <a
-            href="https://www.jonnymckenna.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Jonny McKenna
-          </a>
-        </footer>
-      </FadeIn>
+      <Adsense
+        client="ca-pub-7640562161899788"
+        slot="7259870550"
+        adTest={
+          !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+            ? "off"
+            : "on"
+        }
+      />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => (
+  <div className="footer">
+    Created by{" "}
+    <a
+      href="https://www.jonnymckenna.com/"
+      target="_blank"
+      rel="noopener noreferrer"
+      onClick={() => {}}
+    >
+      Jonny McKenna
+    </a>
+  </div>
+);
+
+export default Footer;
